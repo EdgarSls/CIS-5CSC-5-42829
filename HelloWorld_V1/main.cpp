cda5064b37915400885bddf724cd1075b9219629
EdgarSls <edgarsls792@gmail.com>
2016-02-22 21:36:43 -0800

    Signed-off-by: EdgarSls <edgarsls792@gmail.com>

---
+++ HelloWorld_V1/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Edgar Salas
+ * first program 
+ * Created on February 17, 2016, 9:45 AM
+ */
+//System libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constant
+
+//Function Prototypes 
+
+// Execution Begins Here:
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Declare variables 
+    
+    //Initialize variables
+    
+    //Map the inputs to the outputs
+    
+    //Output the results 
+    cout<<"Hello WORLD"<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
+
